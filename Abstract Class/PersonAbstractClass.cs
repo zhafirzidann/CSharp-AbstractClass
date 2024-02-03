a3c13ac57eab365fcd8f74dc246473a42f171d4b
Zhafir Zidan <gerhana.total753@gmail.com>
2024-02-03 14:58:31 +0700

    first

---
+++ Abstract Class/PersonAbstractClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Apstract_Class_Interface
+{
+
+    public abstract class Person
+    {
+
+        private String name;
+        private String adress;
+
+
+        public Person(String name)
+        {
+            this.name = name;
+
+        }
+        public String getName()
+        {
+            return this.name;
+        }
+        public String changeAdress(String adress)
+        {
+            return this.adress = adress;
+        }
+        public void PoinActive()
+        {
+            Console.WriteLine("Poin keaktifan dua kali lipat diberikan kepada " + this.name);
+        }
+        public void promote(int percentage)
+        {
+            Console.WriteLine("Mahasiswa yang mendapat poin keaktifan " + this.name);
+            PoinActive();
+            Point(percentage);//coling abstract method that not yet implemented
+        }
+        public abstract Boolean Point(int percent);//we define abstract method
+
+    }
+
+}
---
+++ Abstract Class/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Apstract_Class_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Nyobain Abstract Bersama Mahasiswa AMIKOM");
+            Person[] persons = new Person[3];
+            persons[0] = new Employee("Faat");
+            persons[1] = new Employee("Dhendy");
+            persons[2] = new Contractor("Zidan");
+
+            foreach (Person p in persons)
+            {
+                p.promote(15);
+            }
+        }
+    }
+    public class Employee : Person
+    {
+
+        public Employee(String name) : base(name) { }
+
+
+        public override Boolean Point(int percent)
+        {
+            Console.WriteLine($"Mendapat poin keaktifan sebesar {percent} % " + getName());
+            return true;
+
+
+        }
+    }
+    public class Contractor : Person
+    {
+
+        public Contractor(String name) : base(name) { }
+
+        public override Boolean Point(int percent)
+        {
+            if (percent > 5)
+            {
+                Console.WriteLine($"Tidak mendapat poin keaktifan karena jawaban salah : " + getName());
+                return false;
+            }
+            else
+            {
+                Console.WriteLine($"Tidak mendapat poin keaktifan karena jawaban salah : " + getName());
+                return true;
+            }
+        }
+
+    }
+
+}
